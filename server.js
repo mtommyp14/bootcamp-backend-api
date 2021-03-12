60567f38dcb0748c2a6cdfa7f9734bb977284586
mtommyp14 <mtommypras14@gmail.com>
2021-03-12 17:40:53 -0500

    add DB

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//Env
+dotenv.config({
+  path: './config/config.env',
+});
 
 //router file
 const bootcamp = require('./routes/bootcamp');
 
-dotenv.config({ path: './config/config.env' });
-
 const app = express();
 
+connectDB();
+
 //Middleware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 //Mount Router
@@ -19,6 +25,13 @@ app.use('/api/v1/bootcamp', bootcamp);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+//Hadle Error
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.massage}`);
+  //Close Sever & Exir Process
+  server.close(() => process.exit(1));
 });
